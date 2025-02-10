bac5d4f1b510f69ad43d573ef95f15a4c6f8a86b
Anshuman Mishra <anshuman0427@gmail.com>
2025-02-10 15:10:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MarketingPage from './MarketingPage';
@@ -18,6 +20,8 @@ function App() {
           <Route path="/refund-policy" element={<RefundPolicy />} />
           <Route path="/contact" element={<ContactUs />} />
         </Routes>
+        <Analytics />  {/* Add Vercel Analytics here */}
+        <SpeedInsights/>
       </div>
     </Router>
   );
